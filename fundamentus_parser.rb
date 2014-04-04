9b470c69b81cf6dab2ef88b2bc4995e8e8f1fbbf
Vinicius Pinto <viniciusweb@gmail.com>
2014-04-04 01:20:51 -0300

    parser and test

--- fundamentus_parser.rb
+++ fundamentus_parser.rb
@@ -19,32 +19,56 @@ class FundamentusParser
       @doc = Nokogiri::HTML.parse(content)
       parsed = {
         :market_cap => parse_market_cap,
-        :last_processed => parse_last_processed
+        :last_processed => parse_last_processed,
+        :net_debt => parse_net_debt,
+        :net_assets => parse_net_assets,
+        :yearly_net_income => parse_yearly_net_income,
+        :yearly_net_profit => parse_yearly_net_profit
       }
     end
 
     def parse_market_cap
-      label_td = find_td_with_label('Valor de mercado')
-      if label_td
-        data = label_td.next_element().css('span.txt').first.text
-      end
+      read_value_with_label('Valor de mercado')
     end
 
     def parse_last_processed
-      label_td = find_td_with_label('Últ balanço processado')
+      read_value_with_label('Últ balanço processado')
+    end
+
+    def parse_net_debt
+      read_value_with_label('Dív. Líquida')
+    end
+
+    def parse_net_assets
+      read_value_with_label('Patrim. Líq')
+    end
+
+    def parse_yearly_net_income
+      read_value_with_label('Receita Líquida')
+    end
+
+    def parse_yearly_net_profit
+      read_value_with_label('Lucro Líquido')
+    end
+
+    def read_value_with_label(label, index = 0)
+      label_td = find_td_with_label(label, index)
       if label_td
         data = label_td.next_element().css('span.txt').first.text
       end
     end
 
-    def find_td_with_label(label)
+    def find_td_with_label(label, index = 0)
       td = nil
-      @doc.css('body.detalhes div.conteudo td.w2 span.txt').each do |item|
+      count = 0
+      @doc.css('body.detalhes div.conteudo td span.txt').each do |item|
         if item.content.strip == label
-          td = item.parent()
+          if count == index
+            return item.parent()
+          end
+          count += 1
         end
       end
-      td
     end
 
 end
--- test/fundamentus_parser_test.rb
+++ test/fundamentus_parser_test.rb
@@ -5,11 +5,31 @@ class FundamentusParserTest < MiniTest::Unit::TestCase
   def setup
     @parser = FundamentusParser.new
     @page = File.open(File.expand_path('vale5.html', File.dirname(__FILE__) + '/data/')).read
+    @parsed = @parser.parse(@page)
   end
 
   def test_parse_market_cap
-    parsed = @parser.parse(@page)
-    parsed.first[:market_cap].must_equal '157.525.000.000'
+    @parsed.first[:market_cap].must_equal '157.525.000.000'
+  end
+
+  def test_parse_last_processed
+    @parsed.first[:last_processed].must_equal '31/12/2013'
+  end
+
+  def test_parse_net_debt
+    @parsed.first[:net_debt].must_equal '56.511.800.000'
+  end
+
+  def test_parse_net_assets
+    @parsed.first[:net_assets].must_equal '148.346.000.000'
+  end
+
+  def test_parse_yearly_net_income
+    @parsed.first[:yearly_net_income].must_equal '101.490.000.000'
+  end
+
+  def test_parse_yearly_net_profit
+    @parsed.first[:yearly_net_profit].must_equal '115.091.000'
   end
 
 end
